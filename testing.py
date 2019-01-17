94de7b1aab928f8e2861254d06ca17a342f85df0
DM1298 <david.martos9.7@gmail.com>
2019-01-18 00:10:43 +0100

    Testenado

--- testing.py
+++ testing.py
@@ -3,10 +3,4 @@ def testing_funct(veces):
     for i in range(0,veces):
         print("Heylisten")
     return 1
-
-j=testing_funct(5)
-print(j)
-
-
-test=["Juana",6,"Juan",7]
-print(test[:])
+asklfj
