fa7ce4c2dfd8a4438dd2e20e4cf11ab440f69748
DM1298 <david.martos9.7@gmail.com>
2019-01-18 00:05:29 +0100

    Testing 0.0

---
+++ main.py
@@ -0,0 +1,19 @@
+#Pruebas
+#for
+j=5
+for i in range(0,j):
+    print("Hey Listen")
+#while
+i = 0
+while i < j:
+    print("A settlement needs your help")
+    i = i + 1
+
+#funcion
+
+
+
+
+
+
+def function_test
---
+++ testing.py
@@ -0,0 +1,12 @@
+
+def testing_funct(veces):
+    for i in range(0,veces):
+        print("Heylisten")
+    return 1
+
+j=testing_funct(5)
+print(j)
+
+
+test=["Juana",6,"Juan",7]
+print(test[:])
