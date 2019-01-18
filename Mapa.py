9a40a9412a0db816fd943dad5175cab8bd5b3ced
DM1298 <david.martos9.7@gmail.com>
2019-01-18 05:24:38 +0100

    Directorio de partida y de cofres creado
    Casillas de mapa generadas segun caracter de introduccion

--- Mapa.py
+++ Mapa.py
@@ -1,3 +1,45 @@
+def genera_cofre(n_cofre,path_cofre):
+    path_cofre=path_cofre+n_cofre+'.txt'
+    items=[]
+    fd_cofre=open(path)
+    n_items=fd_cofre.readline()
+    n_items.replace('\n','0')
+    n_items=int(n_items)
+    for i in range(0,n_items):
+        item=fd_cofre.readline()
+        item=item[:-1]
+        items.append(item)
+    return items
+
+#Genera/añade los atributos de la casilla segun la 'letra' pasada por parametro
+#n_cofre lleva el conteo de los cofres del mapa
+def genera_casilla(letra,n_cofre,path_cofre):
+    if letra=='#':      #MURO
+        casilla= {
+            'c' : letra,
+            'pisable': False
+        }
+    elif letra==' ':    #AIRE
+        casilla= {
+            'c': letra,
+            'pisable': True
+        }
+    elif letra=='E':    #ENEMIGO
+        casilla= {
+            'c': letra,
+            'pisable': False
+        }
+    elif letra=='C':    #COFRE
+        casilla= {
+            'c': letra,
+            'pisable': False,
+            'n_cofre': n_cofre,
+            'contenido': genera_cofre(n_cofre,path_cofre)
+        }
+
+    return casilla
+
+#Genera la matriz del mapa a partir de archivo txt
 def genera_mapa(path):
     fd=open(path,'r')
     largo=fd.readline()
@@ -7,13 +49,30 @@ def genera_mapa(path):
     ancho.replace('\n','0')
     ancho=int(ancho)
     matriz=[]
+    n_cofre=0
     for i in range(largo):
-        matriz.append(['0']*ancho)
+        matriz.append([0]*ancho)
     for i in range(0,largo):
         for j in range(0,ancho+1):
             casilla=fd.read(1)
             if casilla!='\n':
-                matriz[i][j]=casilla
+                s_casilla=genera_casilla(casilla,n_cofre,path+'/cofres')
+                if casilla == 'C':
+                    n_cofre += 1
+                matriz[i][j]=s_casilla
     fd.close()
     return matriz
-genera_mapa("./mapa1.txt")
+
+def ancho_mapa(mapa):
+    return len(mapa[0])
+
+def largo_mapa(mapa):
+    return len(mapa)
+
+def imprime_mapa(mapa):
+    ancho=ancho_mapa(mapa)
+    largo=largo_mapa(mapa)
+    for i in range(0,largo):
+        for j in range(0,ancho):
+            print(mapa[i][j].get('c'),end='')
+        print()
--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import Menuses
 import Mapa
+
 i = Menuses.menuses(0)
 if i == 0:
     print('NewGame()')
 elif i == 1:
-    print('LoadGame()')
+    partida='./partida/mapa1.txt'
+    mapa=Mapa.genera_mapa(partida)
+    Mapa.imprime_mapa(mapa)
 else:
     print('Salir')
