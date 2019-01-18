4f7332e5ec999eed1bbcf654d3dc0f74742bc475
GeryGery <gerardromerobujalance@hotmail.com>
2019-01-18 05:50:17 +0100

    Menu_Inventario sin acabar

--- main.py
+++ main.py
@@ -8,5 +8,7 @@ elif i == 1:
     partida='./partida/mapa1.txt'
     mapa=Mapa.genera_mapa(partida)
     Mapa.imprime_mapa(mapa)
+    inventario_de_pega = ["Llibertat","Presos","Politics"]
+    Menuses.menu_inventario(inventario_de_pega, mapa)
 else:
     print('Salir')
